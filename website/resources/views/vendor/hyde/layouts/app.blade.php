9aec23a1da888808c2fce01064c00731aa0f4662
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2023-12-13 15:50:38 +0200

    chore: Publish the app Blade layout
    Command used:
    
        php hyde publish:views layouts
    
    Then the layouts were deleted again, except for the app layout.

---
+++ website/resources/views/vendor/hyde/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ config('hyde.language', 'en') }}">
+
+<head>
+  @include('hyde::layouts.head')
+</head>
+
+<body class="flex min-h-screen flex-col overflow-x-hidden dark:bg-gray-900 dark:text-white"
+      id="app"
+      x-data="{ navigationOpen: false }"
+      x-on:keydown.escape="navigationOpen = false;">
+  @include('hyde::components.skip-to-content-button')
+  @include('hyde::layouts.navigation')
+
+  <section>
+    @yield('content')
+  </section>
+
+  @include('hyde::layouts.footer')
+
+  @include('hyde::layouts.scripts')
+</body>
+
+</html>
