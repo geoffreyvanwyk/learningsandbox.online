f1962b7a54ff70652c8038876848d0deff0f53c8
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2023-12-13 22:20:26 +0200

    chore: Remove dark mode from Tailwind

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,7 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    darkMode: 'class',
     content: [
         './_pages/*.blade.php',
         './resources/views/**/*.blade.php',
