1d2fed25952c5c8049c95cb1f49af809fcb0ba16
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2023-12-14 18:30:46 +0200

    fix: Line-awesome icon in navigation brand not loaded

--- files/website/resources/views/vendor/hyde/components/navigation/navigation-brand.blade.php
+++ files/website/resources/views/vendor/hyde/components/navigation/navigation-brand.blade.php
@@ -1,5 +1,5 @@
 <a class="flex items-center space-x-2 px-4 font-bold"
    href="{{ Routes::get('index') }}"
    aria-label="Home page">
-  <i class="fas fa-graduation-cap text-4xl"></i> <span>{{ config('hyde.name', 'HydePHP') }}</span>
+  <i class="las la-graduation-cap text-4xl"></i> <span>{{ config('hyde.name', 'Learning Sandbox Online') }}</span>
 </a>
